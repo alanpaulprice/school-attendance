7f82a82eb57086affdda13969d35cbe3c1a5456a
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2017-12-04 17:05:30 +0000

    css changes, finished off the functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // ===== MODEL =====
 let model = {
+  // Invents random attendance/days missed data for each student
+  // included for demonstration purposes
   init: () => {
     model.records.forEach((item) => {
       let checkCount = 0;
@@ -10,7 +12,6 @@ let model = {
 
         if (!state) checkCount++;
       }
-
       item.daysMissed = checkCount;
     });
   },
@@ -48,6 +49,21 @@ let octopus = {
   init: () => {
     model.init();
     view.init(model.records);
+  },
+
+  checkboxStateChange: (index, i, checked) => {
+    // if a checkbox state was altered, adjusts records accordingly
+    let daysMissed = document.getElementById("days-missed-readout-" + index);
+    
+    if (checked) {
+      model.records[index].attendance[i] = true;
+      model.records[index].daysMissed--;
+      daysMissed.textContent--;
+    } else {
+      model.records[index].attendance[i] = false;
+      model.records[index].daysMissed++;
+      daysMissed.textContent++;
+    }
   }
 }; // ===== OCTOPUS =====
 // ===== ===== ===== ===== =====
@@ -56,44 +72,50 @@ let view = {
   init: (records) => {
 
     let table = document.getElementById("table");
-
+    // Creates a full row structure for each student
     records.forEach((item, ind) => {
       // ===== CREATE ROW =====
       let tableRow = document.createElement("div");
-      tableRow.id = "table-student-row-" + ind;
+      tableRow.id = "table-row-student" + ind;
       tableRow.classList.add("table-row");
       table.appendChild(tableRow);
 
       // ===== CREATE NAME COL =====
-      let nameCol = document.createElement("div");
-      nameCol.classList.add("name-col");
-      nameCol.textContent = item.name;
-      tableRow.appendChild(nameCol);
+      let colName = document.createElement("div");
+      colName.classList.add("table-col-name");
+      colName.textContent = item.name;
+      tableRow.appendChild(colName);
 
       // ===== CREATE DAYS COL =====
-      let daysCol = document.createElement("div");
-      daysCol.classList.add("days-col");
-      tableRow.appendChild(daysCol);
+      let colDays = document.createElement("div");
+      colDays.classList.add("table-col-days");
+      tableRow.appendChild(colDays);
 
       // ===== DIV / CHECKBOX =====
       for (let i = 0; i < 12; i++) {
         let div = document.createElement("div");
-        daysCol.appendChild(div);
+        colDays.appendChild(div);
 
         let checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
+
         if (item.attendance[i]) {
           checkbox.checked = true;
         }
+
+        checkbox.addEventListener("click", () => {
+          octopus.checkboxStateChange(ind, i, checkbox.checked);
+        });
+
         div.appendChild(checkbox);
       }
 
       // ===== CREATE DAYS MISSED COL =====
-      let daysMissedCol = document.createElement("div");
-      daysMissedCol.classList.add("days-missed-col");
-      daysMissedCol.textContent = item.daysMissed;
-      tableRow.appendChild(daysMissedCol);
-
+      let colDaysMissed = document.createElement("div");
+      colDaysMissed.id = "days-missed-readout-" + ind;
+      colDaysMissed.classList.add("table-col-days-missed");
+      colDaysMissed.textContent = item.daysMissed;
+      tableRow.appendChild(colDaysMissed);
     });
   }
 }; // ===== VIEW =====
